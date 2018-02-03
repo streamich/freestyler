564434ca567dbacabaa86496ae5d87072f371547
Vadim Dalecky <streamich@gmail.com>
2018-02-03 21:52:14 +0000

    chore: add clean scripts

--- packages/freestyler-util/supportsCssVariables.js
+++ packages/freestyler-util/supportsCssVariables.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var supportsCssVariables = function () { return window.CSS && CSS.supports && CSS.supports('--a', '0'); };
+var supportsCssVariables = function () { return (typeof window === 'object') && window.CSS && CSS.supports && CSS.supports('--a', '0'); };
 exports.default = supportsCssVariables();
--- packages/freestyler-util/supportsCssVariables.ts
+++ packages/freestyler-util/supportsCssVariables.ts
@@ -1,3 +1,4 @@
-const supportsCssVariables = () => (window as any).CSS && CSS.supports && CSS.supports('--a', '0');
+const supportsCssVariables = () =>
+    typeof window === 'object' && (window as any).CSS && CSS.supports && CSS.supports('--a', '0');
 
 export default supportsCssVariables();
