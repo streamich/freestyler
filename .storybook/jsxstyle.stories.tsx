d40ddbae3b36a35cb4f59fb17beeb147aba65140
Vadim Dalecky <streamich@gmail.com>
2018-02-21 01:33:10 +0000

    chore: add test story

--- .storybook/jsxstyle.stories.tsx
+++ .storybook/jsxstyle.stories.tsx
@@ -4,7 +4,9 @@ const {action} = require('@storybook/addon-actions');
 const {linkTo} = require('@storybook/addon-links');
 import Example1 from './jsxstyle/Example1';
 import Example2 from './jsxstyle/Example2';
+import Example3 from './jsxstyle/Example3';
 
 storiesOf('jsxstyle', module)
   .add('Button', () => <Example1 />)
   .add('Custom blocks', () => <Example2 />)
+  .add('Spinner', () => <Example3 w='20px' h='20px' />)
