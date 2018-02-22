887adad5e43f122b3f296a77665d8e9b6935548c
Vadim Dalecky <streamich@gmail.com>
2018-02-22 11:58:51 +0000

    feat: remove facc()

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,9 @@ import css from './react/css';
 import hoc from './react/hoc';
 import Component from './react/Component';
 import hyperstyle from './react/hyperstyle';
-import facc from './react/facc';
 import hot from './react/hot';
 
-export {rule, StyleSheet, styled, css, hoc, Component, hyperstyle, facc, hot};
+export {rule, StyleSheet, styled, css, hoc, Component, hyperstyle, hot};
 
 export * from './react/styleit';
 export * from './react/jsxstyle';
