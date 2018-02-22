35ad4e035ceced9a73138595cf51b8d5c351049c
Vadim Dalecky <streamich@gmail.com>
2018-02-22 12:38:01 +0000

    feat: remove hyperstyle() and add wrap() interface

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import rule from './rule';
 import StyleSheet from './StyleSheet';
-import styled from './styled';
+import wrap from './react/wrap';
+import styled from './react/styled';
 import css from './react/css';
 import hoc from './react/hoc';
 import Component from './react/Component';
-import hyperstyle from './react/hyperstyle';
 import jsxstyle from './react/jsxstyle';
 
-export {rule, StyleSheet, styled, css, hoc, Component, jsxstyle};
+export {rule, StyleSheet, wrap, styled, css, hoc, Component, jsxstyle};
